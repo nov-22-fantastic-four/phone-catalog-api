97ebbcb5cf08dfd3e0830c83260e729384e4d6d7
Nazarii Kyselevych <nazarkysel@gmail.com>
2023-03-21 12:13:14 +0200

    updated model, added /discount route for products

--- src/routers/product.router.ts
+++ src/routers/product.router.ts
@@ -4,8 +4,12 @@ import * as productController from '../controllers/product.controller';
 const router = express.Router();
 
 router.get('/', productController.getAll);
+
 router.get('/count', productController.getCount);
 router.get('/new', productController.getNew);
+router.get('/discount', productController.getDiscounted);
+
 router.get('/:id', productController.getOne);
+router.get('/:id/recommended', productController.getNew);
 
 export default router;
